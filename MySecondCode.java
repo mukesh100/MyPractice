f51549574c7a89f8500da6b23496a2249bf72484
Mukesh Kumar Purbey <mukesh.purbey@paytm.com>
2021-06-06 22:20:03 +0530

    Second code

---
+++ MySecondCode.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+public class
+{
+    public static void main(String [] args)
+    {
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter your Roll NUmber")
+       int roll no. = sc.nextInt();
+       System.out.println("Enter your Name");
+       string name = sc.nextLine();
+       System.out.println(roll no. + " " +name);
+
+
+    }
+}
